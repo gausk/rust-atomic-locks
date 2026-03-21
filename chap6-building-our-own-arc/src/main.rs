404711df1fde7184d65d16961423b23b398ee6d1
Gaurav Kumar <gauravsingh12341@gmail.com>
2026-03-21 20:03:24 +0530

    arc complete

---
+++ chap6-building-our-own-arc/src/main.rs
@@ -0,0 +1,104 @@
+use std::ptr::NonNull;
+use std::sync::atomic::{fence, AtomicUsize, Ordering};
+use std::ops::Deref;
+use std::sync::atomic::Ordering::{Acquire, Relaxed};
+
+struct ArcData<T> {
+    data: T,
+    ref_count: AtomicUsize,
+}
+
+pub struct Arc<T> {
+    ptr: NonNull<ArcData<T>>,
+}
+
+unsafe impl<T: Send + Sync> Send for Arc<T> {}
+unsafe impl<T: Send + Sync> Sync for Arc<T> {}
+
+impl<T> Arc<T> {
+    pub fn new(data: T) -> Arc<T> {
+        Arc {
+            ptr: NonNull::from(Box::leak(Box::new(ArcData {
+                ref_count: AtomicUsize::new(1),
+                data,
+            }))),
+        }
+    }
+
+    fn data(&self) -> &ArcData<T> {
+        unsafe { self.ptr.as_ref() }
+    }
+
+    fn clone(&self) -> Self {
+        if self.data().ref_count.fetch_add(1, Relaxed) > usize::MAX / 2 {
+            std::process::abort();
+        }
+        Arc {
+            ptr: self.ptr,
+        }
+    }
+}
+
+impl<T> Deref for Arc<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.data().data
+    }
+}
+
+impl<T> Drop for Arc<T> {
+    fn drop(&mut self) {
+        // TODO: Memory ordering.
+        if self.data().ref_count.fetch_sub(1, Ordering::Release) == 1 {
+            fence(Acquire);
+            unsafe {
+                drop(Box::from_raw(self.ptr.as_ptr()));
+            }
+        }
+    }
+}
+
+#[test]
+fn test() {
+    static NUM_DROPS: AtomicUsize = AtomicUsize::new(0);
+
+    struct DetectDrop;
+
+    impl Drop for DetectDrop {
+        fn drop(&mut self) {
+            NUM_DROPS.fetch_add(1, Relaxed);
+        }
+    }
+
+    // Create two Arcs sharing an object containing a string
+    // and a DetectDrop, to detect when it's dropped.
+    let x = Arc::new(("hello", DetectDrop));
+    let y = x.clone();
+
+    // Send x to another thread, and use it there.
+    let t = std::thread::spawn(move || {
+        assert_eq!(x.0, "hello");
+    });
+
+    // In parallel, y should still be usable here.
+    assert_eq!(y.0, "hello");
+
+    // Wait for the thread to finish.
+    t.join().unwrap();
+
+    // One Arc, x, should be dropped by now.
+    // We still have y, so the object shouldn't have been dropped yet.
+    assert_eq!(NUM_DROPS.load(Relaxed), 0);
+
+    // Drop the remaining `Arc`.
+    drop(y);
+
+    // Now that `y` is dropped too,
+    // the object should've been dropped.
+    assert_eq!(NUM_DROPS.load(Relaxed), 1);
+}
+
+fn main() {
+    println!("Hello, world!");
+}
